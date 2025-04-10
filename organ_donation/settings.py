1aad41edb39e37d9989b72a225599f5b7bf2da01
tharun-vara18 <323103311058@gvpce.ac.in>
2025-04-10 15:20:44 +0530

    Update settings.py

--- organ_donation/settings.py
+++ organ_donation/settings.py
@@ -13,7 +13,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 SECRET_KEY = 'django-insecure-9o5_-4mdzdi6ypmo!7p#k9$)&vkty)iz8)f_3^%g*_drz%h3k6'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'https://organ-donation-system-gone.onrender.com'
+
+]
 
 # Application definition
 INSTALLED_APPS = [
